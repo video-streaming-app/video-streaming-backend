22f7b0701043e3b672018686fcab59c3aa71b87e
Cheong <82318965+leicester70@users.noreply.github.com>
2024-03-11 00:19:34 +0800

    init

---
+++ bootleg-netflix/src/test/java/com/example/bootlegnetflix/BootlegNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootlegnetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootlegNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
